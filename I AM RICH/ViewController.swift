7ff57ecd11c4271a8d9b51aaf6c99ef99d7958ad
David Davidov <D90X@Davids-Air-5.fios-router.home>
2018-03-31 15:37:36 -0400

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by David Davidov on 11/16/17.
+//  Copyright © 2017 David Davidov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var iAmRichLogo: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //
+    }
+
+}
+
